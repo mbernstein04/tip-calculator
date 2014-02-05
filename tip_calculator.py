64de23ccffacf355beccfe2b15eec4d7e1b3bedf
Michael Bernstein <mbernstein04@gmail.com>
2014-02-05 18:16:08 -0500

    Add sys.argv refractor

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,15 +1,14 @@
-meal = raw_input("How much does your meal cost before tax? ")
-tax = raw_input("How much is sales tax? ")
-tip = raw_input("What would you like to tip? ")
+import sys
 
-meal_converted = float(meal)
-tax_converted = float(tax)
-tip_converted = float(tip)
+meal_converted = float(sys.argv[1])
+tax_converted = float(sys.argv[2])
+tip_converted = float(sys.argv[3])
 
 tax_value = meal_converted * tax_converted
 meal_with_tax = tax_value + meal_converted
 tip_value = meal_converted * tip_converted
 total = meal_with_tax + tip_value
+
 
 print "The base cost of the meal is %r" % ("{:.2f}".format(meal_converted))
 print "The dollar value of tax on the meal is %r" % ("{:.2f}".format(tax_value))
